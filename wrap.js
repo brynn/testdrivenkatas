341a1cb4a3f183d687829d6abbce7b745d5946fc
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-19 12:00:28 -0500

    progress on wrap

--- wrap.js
+++ wrap.js
@@ -1,27 +1,30 @@
 const wrap = (line, maxLen) => {
-
   let finalStr = '';
 
   if (line.length <= maxLen) {
     return line;
   } else {
-
-    const numLines = Math.floor(line.length/maxLen);
+    const numLines = Math.floor(line.length / maxLen);
+    let idxOfLineBreak = maxLen;
 
     for (let i = 0; i < numLines; i++) {
+      const currentSubstring = line.substring(
+        i * idxOfLineBreak,
+        (i + 1) * (maxLen - 1)
+      );
+      console.log('substring: ', currentSubstring);
 
-      const placeToInsertLineBreak = line.charAt((i+1)*maxLen-1);
-
-
-      if (i === numLines-1) {
-        finalStr += line.substring((i*maxLen));
+      if (i === numLines - 1) {
+        finalStr += line.substring(i * idxOfLineBreak);
         break;
       }
 
-      if (placeToInsertLineBreak === ' ') {
-        finalStr += line.substring((i*maxLen), line.indexOf(placeToInsertLineBreak) + 1);
+      if (line.charAt((i + 1) * (maxLen - 1)) === ' ') {
+        finalStr += currentSubstring;
       } else {
-        const indexOfLastSpace = line.substring((i*maxLen), line.indexOf(placeToInsertLineBreak) + 1).lastIndexOf(' ');
+        const idxOfLastSpace = currentSubstring.lastIndexOf(' ');
+        finalStr += line.substring(i * idxOfLineBreak, idxOfLastSpace + 1);
+        idxOfLineBreak = idxOfLastSpace + 1;
       }
       finalStr += '\n';
     }
